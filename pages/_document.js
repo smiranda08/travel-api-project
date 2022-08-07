848f48b71514051de2e79ced87e91119eb7c8452
smiranda08 <stefanmiranda08@gmail.com>
2022-08-08 00:58:49 +1000

    using Script tags instead in document.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,10 +15,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
           />
-          <script
+          <Script
             async
             src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAbn_WBgbM69aXq3XFtFgipqoacEIOIW1U"
-          ></script>
+          ></Script>
         </Head>
         <body>
           <Main />
