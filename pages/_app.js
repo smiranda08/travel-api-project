19b8fa27714475fc83e927ba1e5db37461ae3f42
smiranda08 <stefanmiranda08@gmail.com>
2022-08-08 01:14:22 +1000

    fix: <script> component in _app.js 2

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,10 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      <script
         async
         src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAbn_WBgbM69aXq3XFtFgipqoacEIOIW1U"
-      ></Script>
+      ></script>
       <Component {...pageProps} />
     </>
   )
